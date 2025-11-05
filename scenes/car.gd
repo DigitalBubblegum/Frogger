47505b6e9ab1074c7813960ae0436b748228122b
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2025-11-05 21:07:41 +0530

    added spawming cars

---
+++ scenes/car.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var direction = Vector2.LEFT
+var speed = 2
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if position.x < 0:
+		direction.x = 1
+		$Sprite2D.flip_h = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position+= direction*speed
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
--- scenes/game.gd
+++ scenes/game.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var car_scene: PackedScene = preload("res://scenes/car.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +12,10 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print('player entered')
+
+
+func _on_car_timer_timeout() -> void:
+	var car = car_scene.instantiate() as Area2D
+	var pos_marker = $CarStartPositions.get_children().pick_random() as Marker2D
+	car.position = pos_marker.position
+	add_child(car)
