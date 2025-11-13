55ddad0c9fa28cb43e63b9f5ee949d5952a79c65
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2025-11-13 19:36:27 +0530

    added some objects and randomized the color of moving cars

--- scenes/car.gd
+++ scenes/car.gd
@@ -2,11 +2,17 @@ extends Area2D
 
 var direction = Vector2.LEFT
 var speed = 200
+var colors = [
+	preload("res://graphics/cars/green.png"),
+	preload("res://graphics/cars/red.png"),
+	preload("res://graphics/cars/yellow.png")
+]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if position.x < 0:
 		direction.x = 1
 		$Sprite2D.flip_h = true
+	$Sprite2D.texture = colors.pick_random()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
